e12e39f5370b429a53d459b91ccbfbfb0a739e7f
rogeriodelphi <rogeriodelphi@gmail.com>
2022-06-30 01:54:08 -0300

    Criando endpoint de Prato

--- restaurante/views.py
+++ restaurante/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from restaurante.models import Prato
+from restaurante.serializers import PratoSerializer
+
+class PratoListView(generics.ListAPIView):
+    queryset = Prato.objects.all()
+    serializer_class = PratoSerializer
